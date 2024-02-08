7c70411e8f7a595bac728e7d7ebf537eacf8aa08
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-02-08 15:54:55 -0500

    Task 4a complete

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public float speed = 5;
+    float timer = 0;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (Vector2.left * speed) * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", 1);
+        Destroy(gameObject);
+    }
+}
