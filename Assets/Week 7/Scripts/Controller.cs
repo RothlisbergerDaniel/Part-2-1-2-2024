8a1f9a420ec662bc850e1382e2a52bce72c780f0
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-02-22 15:22:28 -0500

    Task 2 complete

---
+++ Assets/Week 7/Scripts/Controller.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public Slider chargeSlider;
+    float charge;
+    public float maxCharge = 1;
+    Vector2 direction;
+
+    public static FootballGuy selectedPlayer { get; private set; }
+    public static void SetSelectedPlayer(FootballGuy player)
+    {
+        if(selectedPlayer != null)
+        {
+            selectedPlayer.selected(false);
+        }
+        selectedPlayer = player;
+        selectedPlayer.selected(true);
+    }
+
+    private void Update()
+    {
+        if (selectedPlayer == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime;
+            charge = Mathf.Clamp(charge, 0, maxCharge);
+            chargeSlider.value = charge;
+
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedPlayer.transform.position).normalized * charge;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(direction != Vector2.zero)
+        {
+            selectedPlayer.move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+}
--- Assets/Week 7/Scripts/FootballGuy.cs
+++ Assets/Week 7/Scripts/FootballGuy.cs
@@ -6,10 +6,16 @@ public class FootballGuy : MonoBehaviour
 {
 
     SpriteRenderer renderer;
+    public Color selectColor;
+    Rigidbody2D rb;
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        selected(false);
     }
 
     // Update is called once per frame
@@ -20,18 +26,24 @@ public class FootballGuy : MonoBehaviour
 
     private void OnMouseDown()
     {
-        selected(true);
+        Controller.SetSelectedPlayer(this);
+        Debug.Log("Clicked");
     }
 
     public void selected(bool select)
     {
-        if(select)
+        if(select == true)
         {
             renderer.color = Color.red;
         }
         else
         {
-            renderer.color = new Color(180, 0, 0, 255);
+            renderer.color = selectColor;
         }
+    }
+
+    public void move(Vector2 dir)
+    {
+        rb.AddForce(dir * speed);
     }
 }
